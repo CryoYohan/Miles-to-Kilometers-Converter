4445bd443461af3b38973904055a1b758f88d4b3
unknown <FF648@ccs.local>
2025-03-03 15:02:28 +0800

    edited window.py class

--- window.py
+++ window.py
@@ -1,4 +1,15 @@
-from tkinter import Tkinter
+import tkinter
 
- class Window(Tkinter):
-    self.title("Converting Miles to Kilometers")
+class Window():
+    
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.title("Miles to Kilometers Converter")
+        self.window.minsize(width=500,height=300)
+        self.my_label = tkinter.Label(text="Hello World Python", font=("Arial", 24, "bold"))
+        self.my_label.pack()
+
+
+if __name__ == "__main__":
+    start = Window()
+    start.window.mainloop()
